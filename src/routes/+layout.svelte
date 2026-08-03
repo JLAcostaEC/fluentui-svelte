0c8f669a975e9f762734b246da3420a8a48f429f
Jorge Acosta <61467132+JLAcostaEC@users.noreply.github.com>
2026-08-03 10:36:31 -0400

    format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 
 	import { page } from '$app/state';
 	import { resolve } from '$app/paths';
-	import { ModeWatcher } from "mode-watcher";
+	import { ModeWatcher } from 'mode-watcher';
 	import { FluentUISvelte } from '$lib/index.js';
 	import { locales, localizeHref } from '$i18n/runtime.js';
 	import type { Pathname } from '$app/types';
